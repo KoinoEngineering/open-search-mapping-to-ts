a06ff769dad9d5aadcc1f956374ffbbb0db612aa
KoinoEngineering <email@koino.engineering>
2021-11-18 01:17:27 +0000

    cli

--- cli.ts
+++ cli.ts
@@ -1,3 +1,29 @@
-import "~/src";
-// eslint-disable-next-line no-console
-console.log(process.argv);
+import commandLineArgs from "command-line-args";
+import main from "./src";
+import StackError from "./src/error/StackError";
+
+const optionDefinitions: commandLineArgs.OptionDefinition[] = [
+  {
+    name: "host",
+    alias: "h",
+    type: String,
+  },
+  {
+    name: "indexName",
+    alias: "i",
+    type: String,
+  },
+  {
+    name: "outDir",
+    alias: "o",
+    type: String,
+  },
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+if (!options.host) {
+  throw new StackError("cli args has no host");
+}
+
+main(options.host, options.indexName, options.outDir);
---
+++ index.ts
@@ -0,0 +1,5 @@
+import main from "./src";
+const host = "http://localhost:8083";
+const indexName = "rooms-ib-search2-with-schedules";
+const outDir = "src/generated";
+main(host, indexName, outDir);

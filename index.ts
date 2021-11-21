f0301541c0b5baf0b3bacda3bcc71b9f8f4329af
KoinoEngineering <email@koino.engineering>
2021-11-21 23:39:25 +0000

    index.ts にrequiredAllを追加

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import dotEnv from "dotenv";
 dotEnv.config();
 
 if (process.env.MAPPING_URL) {
-  main(process.env.MAPPING_URL, process.env.OUT_FILE);
+  main(process.env.MAPPING_URL, process.env.OUT_FILE, {
+    requiredAll: process.env.REQUIRED_ALL === "true",
+  });
 } else {
   // eslint-disable-next-line no-console
   console.log("MAPPING_URL is not set");
